d8e030756d65be82f560d7154d414f06f1609c83
Nathan Beene <beene.nathan@gmail.com>
2025-09-10 10:49:54 -0500

    [Fix] needed to add base in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/PBS-Editor/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
