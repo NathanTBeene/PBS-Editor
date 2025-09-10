bafc3ee0ffd93a7551ed94108d7878e21af335e0
Nathan Beene <beene.nathan@gmail.com>
2025-09-10 10:55:44 -0500

    attempt to fix base link.

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/PBS-Editor/",
+  base: "/PBS-Editor",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
