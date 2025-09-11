9b6b47a3cac9494b755414512134742df82234a8
Nathan Beene <beene.nathan@gmail.com>
2025-09-11 11:36:58 -0500

    corrected 1.0.1 updates.

--- src/lib/data/versionHistory.ts
+++ src/lib/data/versionHistory.ts
@@ -2,6 +2,7 @@ export const currentVersion = "v.1.0.1";
 
 export const versionHistory: Record<string, string[]> = {
   "v.1.0.1": [
+    "Added database clearing to settings.",
     "Fixed abilities not properly updating.",
     "Fixed base stats being flipped around.",
     "?You may need to clear the database or reimport your data to see the changes.",
