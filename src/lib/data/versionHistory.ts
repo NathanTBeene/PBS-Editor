1392618fa373cb60f06c6398305031e05053894b
NateDev <141785075+NathanTBeene@users.noreply.github.com>
2025-09-16 06:53:25 -0500

    Updated version history.

--- src/lib/data/versionHistory.ts
+++ src/lib/data/versionHistory.ts
@@ -1,6 +1,10 @@
-export const currentVersion = "v.1.0.1";
+export const currentVersion = "v.1.0.2";
 
 export const versionHistory: Record<string, string[]> = {
+  "v.1.0.2": [
+    "Fixed Move PP not being imported correctly.",
+    "?You may need to clear the database or reimport your data to see the changes.",
+  ],
   "v.1.0.1": [
     "Added database clearing to settings.",
     "Fixed abilities not properly updating.",
